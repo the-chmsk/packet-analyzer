578ac995b083dd05ee41bfbd4c3691bf3b4549b2
the-chmsk <chmsk@yahoo.com>
2024-04-03 23:23:35 +0200

    Define options

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  static struct option long_options[] = {
+      {"help", no_argument, NULL, 'h'},
+      {"interface", required_argument, NULL, 'i'},
+      {"port", required_argument, NULL, 'p'},
+      {"tcp", no_argument, NULL, 't'},
+      {"udp", no_argument, NULL, 'u'},
+      {"arp", no_argument, 0, 0},
+      {"icmp4", no_argument, 0, 0},
+      {"icmp6", no_argument, 0, 0},
+      {"igmp", no_argument, 0, 0},
+      {"mld", no_argument, 0, 0}};
+
+  int c;
+  int option_index = 0;
+  while ((c = getopt_long(argc, argv, "hi:p:tu01234", long_options, &option_index)) != -1)
+  {
+    // TODO: Implement option parsing
+  }
+
+  return EXIT_SUCCESS;
+}
